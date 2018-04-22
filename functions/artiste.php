fd22a5c835d8ee6c2527d9f2e9b203750368c896
lanxor <lanxor@debian>
2018-04-22 16:24:53 +0200

    Maj: commentaire

--- functions/artiste.php
+++ functions/artiste.php
@@ -5,7 +5,12 @@
  * Fichier des fonction de gestion des artistes
  */
 
-
+/*
+ * Récupère tout les artistes de la base de données
+ * en les triant par ordre alphabétique par le nom
+ * et ensuite le prenom.
+ * Renvoie le résultat.
+ */
 function recuperer_artiste_tous($db) {
   $req = $db->prepare("SELECT * FROM Artiste ORDER BY nomArtiste, prenomArtiste ASC");
   $req->execute();
@@ -13,6 +18,12 @@ function recuperer_artiste_tous($db) {
   return $res;
 }
 
+/*
+ * Récupère un artiste de la base de données
+ * spécifier par l'identifiant 'idArtiste'.
+ * Renvoie le résultat, il est unique.
+ * Sinon renvoie la valeur null.
+ */
 function recupere_artiste($db, $idArtiste) {
   $req = $db->prepare("SELECT * FROM Artiste WHERE idArtiste=:idArtiste");
   $req->bindParam(':idArtiste', $idArtiste);
@@ -24,6 +35,12 @@ function recupere_artiste($db, $idArtiste) {
   return null;
 }
 
+/*
+ * Ajoute un nouvel artiste à la base de données
+ * avec un nom, prénom, nom de scéne, date de naissance, url d'une image
+ * et une description de l'artiste.
+ * Renvoie si l'opération d'ajout c'est bien exécuté.
+ */
 function ajouter_artiste($db, $nomArtiste, $prenomArtiste, $nomScene, $dateNaissanceArtiste,  $urlImageArtiste, $descriptionArtiste) {
   $req = $db->prepare("INSERT INTO Artiste(nomArtiste, prenomArtiste, nomScene, urlImageArtiste, descriptionArtiste)
       VALUES(:nomArtiste, :prenomArtiste, :nomScene, :urlImageArtiste, :descriptionArtiste);");
@@ -37,6 +54,12 @@ function ajouter_artiste($db, $nomArtiste, $prenomArtiste, $nomScene, $dateNaiss
   return $reqOk;
 }
 
+/*
+ * Modifier un artiste existant dans la base de données
+ * avec un nom, prénom, nom de scéne, date de naissance, url d'une image
+ * et une description de l'artiste.
+ * Renvoie si l'opération de mdoification c'est bien exécuté.
+ */
 function modifier_artiste($db, $idArtiste, $nomArtiste, $prenomArtiste, $nomScene, $dateNaissanceArtiste,  $urlImageArtiste, $descriptionArtiste) {
   $req = $db->prepare("UPDATE Artiste SET nomArtiste=:nomArtiste, prenomArtiste=:prenomArtiste, nomScene=:nomScene, urlImageArtiste=:urlImageArtiste, descriptionArtiste=:descriptionArtiste WHERE idArtiste=:idArtiste;");
   $req->bindParam(':idArtiste', $idArtiste, PDO::PARAM_INT);
@@ -50,6 +73,11 @@ function modifier_artiste($db, $idArtiste, $nomArtiste, $prenomArtiste, $nomScen
   return $reqOk;
 }
 
+/*
+ * Supprime un artiste de la base de données
+ * spécifier par l'identifiant 'idArtiste'.
+ * Renvoie si l'opération de suppression c'est bien exécuté.
+ */
 function supprimer_artiste($db, $idArtiste) {
   $req = $db->prepare("DELETE FROM Artiste WHERE idArtiste=:idArtiste;");
   $req->bindParam(':idArtiste', $idArtiste, PDO::PARAM_INT);
