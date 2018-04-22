062f893dec1eedb737b471264750dc08f6c5b43d
lanxor <lanxor@debian>
2018-04-22 15:51:12 +0200

    Maj: style administration et par défaut

--- administration/index.php
+++ administration/index.php
@@ -17,15 +17,13 @@
     <h1>
       <a href="./index.php">Panneau d'adminsitration</a>
     </h1>
-    <section class="flex flex-center">
-      <a class="button button-blue" href="./adminGestionUser.php">
-        Gestion des utilisateur
-      </a>
-      <a class="button button-blue" href="./adminGestionArtiste.php">
-        Gestion des artistes
-      </a>
+    <section class="">
+        <a class="button button-blue" href="./adminGestionUser.php">
+          Gestion des utilisateur
+        </a><br><br>
+        <a class="button button-blue" href="./adminGestionArtiste.php">
+          Gestion des artistes
+        </a>
     </section>
   </section>
 </main>
-
-<?php include_once(dirname(__FILE__).'/../footer.php'); ?>
