8c9a7fb49f9dd2ecdaba3a66ac5bcc55685d0b85
lanxor <lanxor@debian>
2018-04-25 14:52:29 +0200

    Simplification de la gestion de l'initialisation BD

--- database/database.php
+++ database/database.php
@@ -3,12 +3,7 @@
 include_once(dirname(__FILE__).'/connexion.php');
 include_once(dirname(__FILE__).'/../functions/base.php');
 
-if ( !isset($_GET['name']) || $_GET['name'] != 'admin' ) { leave(); }
-
-$file = file_get_contents('./structure.sql');
-
-echo $file;
-
-$db->exec($file);
+$db->exec(file_get_contents('./structure.sql'));
+$db->exec(file_get_contents('./population.sql'));
 
 ?>
