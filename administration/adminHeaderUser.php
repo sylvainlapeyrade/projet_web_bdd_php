5710054dd62d22ea61934097de3c7be5a13e2055
lanxor <lanxor@debian>
2018-04-25 18:00:40 +0200

    Maj: nouveau header comme adminHeaderArtiste.php

--- administration/adminHeaderUser.php
+++ administration/adminHeaderUser.php
@@ -5,14 +5,21 @@
   <div>
     <a href="adminFormAddUser.php" class="button button-blue t12 button-header">Ajouter un utilisateur</a>
   </div>
-
+  
   <!-- Message d'erreur du formulaire -->
-  <? if ( isset($error) ) { ?>
-  <div class="red"><?php echo $error; ?></div>
-  <? } ?>
-
+  <? if ( isset($erreur) ) { ?>
+  <div class="red"><?php echo $erreur; ?></div>
+  
   <!-- Message du formulaire -->
-  <? if ( isset($message) ) { ?>
+  <? } elseif ( isset($message) ) { ?>
   <div class="green"><?php echo $message; ?></div>
-  <? } ?>
+  
+  <!-- Message en rapport a une opération -->
+  <? } elseif ( isset($_GET['operation']) && $_GET['operation'] == 'ok' ) { ?>
+  <div class="green">L'opération a été effectué.</div>
+  
+  <? } elseif ( isset($_GET['operation']) && $_GET['operation'] == 'ko' ) { ?>
+  <div class="red">L'opération n'a pas pu être effectué.</div>
+  <?php } ?>
+  
 </div>
