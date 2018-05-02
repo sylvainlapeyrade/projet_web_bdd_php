e2c39337b3761703c7f4799a9bda59c0c1cea4ac
Sylvain <sylvain.lapeyrade@hotmail.fr>
2018-05-03 00:08:05 +0200

    Modification album.php Presentation de la page statique avec un exemple d'album

--- album.php
+++ album.php
@@ -20,52 +20,79 @@
         <div class="flex">
             <div id="description-artiste" class="flex-around">
                 <div>
-                    <h1>Nom_Album</h1>
+                    <h1><a>Nom_Album</a>     <a>Date_sortie</h1>
                     <p>
                         Ceci est la description de l'album<br>
-                        A peep at some distant orb has power to raise and purify our thoughts like a strain of sacred music, or a noble picture, or a passage from the grander poets. It always does one good.<br>
+                        Player est le deuxième album du chanteur français M. Pokora sorti le 26 janvier 2006 en France. À sa sortie, l'album se classera à la première place du classement des ventes d'albums avec 31 000 exemplaires. <br>
+                        <br>
                     </p>
                     <p>
-                        Il fait parti des groupes : <a>GroupeA</a>, <a>GroupeB</a>, <a>GroupeC</a>.
+                        Il a été composé par l'artiste : <a>Nom_Artiste</a>
                     </p>
                 </div>
-                <div id="liste-album" class="flex flex-arround">
+                <div id="liste-musique" class="flex flex-arround">
                     <div>
-                        <h4>Albums de l'artiste</h4>
+                        <h4>Morceaux de l'album:</h4>
                         <table>
                             <tr>
                                 <th class="table-head width-250">Nom</th>
                                 <th class="table-head width-250">Année</th>
+                                <th class="table-head width-250">Durée</th>
                             </tr>
                             <tr class="table-lign">
-                                <td>Album 1</td>
-                                <td>2010</td>
+                                <td>Piste n°1</td>
+                                <td><a>De retour</a></td>
+                                <td>3:30</td>
                             </tr>
                             <tr class="table-lign">
-                                <td>Album 2</td>
-                                <td>2010</td>
+                                <td>Piste n°2</td>
+                                <td><a>Player</a></td>
+                                <td>2:50</td>
                             </tr>
                             <tr class="table-lign">
-                                <td>Album 3</td>
-                                <td>2010</td>
-                            </tr>
-                            <tr class="table-lign"><td>Album 3</td><td>2010</td></tr>
-                        </table>
-                    </div>
-                    <div>
-                        <h4>Récompense de l'artiste</h4>
-                        <table>
-                            <tr>
-                                <th class="table-head width-250">Nom</th>
-                                <th class="table-head width-250">Date</th>
+                                <td>Piste n°3</td>
+                                <td><a>Oh la la la</a></td>
+                                <td>3:01</td>
                             </tr>
                             <tr class="table-lign">
-                                <td>Récompense 1</td>
-                                <td>2010</td>
+                                <td>Piste n°4</td>
+                                <td><a>Ce soir je lui dis tout</a></td>
+                                <td>3:51</td>
                             </tr>
                             <tr class="table-lign">
-                                <td>Récompense 2</td>
-                                <td>2010</td>
+                                <td>Piste n°5</td>
+                                <td><a>Mal de guerre</a></td>
+                                <td>3:56</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°6</td>
+                                <td><a>L'enfer du samedi soir</a></td>
+                                <td>4:08</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°7</td>
+                                <td><a>Interlude</a></td>
+                                <td>1:01</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°8</td>
+                                <td><a>Cynthia</a></td>
+                                <td>2:51</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°9</td>
+                                <td><a>STP</a></td>
+                                <td>3:29</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°10</td>
+                                <td><a>Cette Fille</a></td>
+                                <td>3:14</td>
+                            </tr>
+                            <tr class="table-lign">
+                                <td>Piste n°11</td>
+                                <td><a>Regarde maman / Je suis prêt pour la bataille</a></td>
+                                <td>3:29</td>
                             </tr>
                         </table>
                     </div>
@@ -73,41 +100,6 @@
             </div>
             <div>
                 <img id="imageArtiste" src="styles/mpokora.htm">
-            </div>
-        </div>
-
-        <!-- Albums et Musique de l'artiste -->
-        <div>
-
-            <hr>
-            <div id="liste-musique">
-                <h4>Musique de l'artiste</h4>
-                <table>
-                    <tr>
-                        <th class="table-head width-300">Titre</th>
-                        <th class="table-head width-300">Durée</th>
-                        <th class="table-head width-300">Date</th>
-                        <th class="table-head width-600">Descritpion</th>
-                    </tr>
-                    <tr class="table-lign">
-                        <td class="">Chanson 1</td>
-                        <td>2 minutes 64</td>
-                        <td>02/19/1978</td>
-                        <td>Ceci est une description de la musique</td>
-                    </tr>
-                    <tr class="table-lign">
-                        <td>Album1</td>
-                        <td>30 sec</td>
-                        <td>02/19/1978</td>
-                        <td>Ceci est une description de la musique</td>
-                    </tr>
-                    <tr  class="table-lign">
-                        <td>Album1</td>
-                        <td>1 heures</td>
-                        <td>02/19/1978</td>
-                        <td>Ceci est une description de la musique</td>
-                    </tr>
-                </table>
             </div>
         </div>
     </section>
