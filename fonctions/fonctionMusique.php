2b29aa4ebe0e8f995889af2fa975eb1a5b0a4156
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-07 16:46:37 +0200

    Gestion musique fonctions

--- fonctions/fonctionMusique.php
+++ fonctions/fonctionMusique.php
@@ -18,7 +18,6 @@ function recuperer_musique($db, $idMusique) {
   return null;
 }
 
-
 function ajouter_musique($db, $titreMusique, $dureeMusique, $dateMusique, $descriptionMusique) {
   $req = $db->prepare("INSERT INTO Musique(titreMusique, dureeMusique, descriptionMusique)
       VALUES(:titreMusique, :dureeMusique, :descriptionMusique);");
@@ -44,6 +43,55 @@ function modifier_musique($db, $idMusique, $titreMusique, $dureeMusique, $dateMu
 function supprimer_musique($db, $idMusique) {
   $req = $db->prepare("DELETE FROM Musique WHERE idMusique=:idMusique;");
   $req->bindParam(':idMusique', $idMusique, PDO::PARAM_INT);
+  $reqOk = $req->execute();
+  return $reqOk;
+}
+
+function recuperer_composer_musique($db, $idMusiqueCoMu) {
+  $req = $db->prepare("SELECT * FROM Composer_Musique WHERE idMusiqueCoMu=:idMusiqueCoMu");
+  $req->bindParam(':idMusiqueCoMu', $idMusiqueCoMu, PDO::PARAM_INT);
+  $req->execute();
+  $res = $req->fetchAll();
+  return $res;
+}
+
+function ajouter_composer_musique($db, $idMusiqueCoMu, $idArtisteCoMu) {
+  $req = $db->prepare("INSERT INTO Composer_Musique(idMusiqueCoMu, idArtisteCoMu)
+      VALUES(:idMusiqueCoMu, :idArtisteCoMu);");
+  $req->bindParam(':idMusiqueCoMu', $idMusiqueCoMu, PDO::PARAM_INT);
+  $req->bindParam(':idArtisteCoMu', $idArtisteCoMu, PDO::PARAM_INT);
+  $reqOk = $req->execute();
+  return $reqOk;
+}
+
+function supprimer_composer_musique_tous($db, $idMusiqueCoMu) {
+  $req = $db->prepare("DELETE FROM Composer_Musique WHERE idMusiqueCoMu=:idMusiqueCoMu;");
+  $req->bindParam(':idMusiqueCoMu', $idMusiqueCoMu, PDO::PARAM_INT);
+  $reqOk = $req->execute();
+  return $reqOk;
+}
+
+function recuperer_assembler_album($db, $idMusiqueAa) {
+  $req = $db->prepare("SELECT * FROM Composer_Album WHERE idMusiqueAa=:idMusiqueAa");
+  $req->bindParam(':idMusiqueAa', $idMusiqueAa, PDO::PARAM_INT);
+  $req->execute();
+  $res = $req->fetchAll();
+  return $res;
+}
+
+function ajouter_assembler_album($db, $idMusiqueAa, $idAlbumAa, $numeroPiste) {
+  $req = $db->prepare("INSERT INTO Composer_Album(idMusiqueAa, idAlbumAa, numeroPiste)
+      VALUES(:idMusiqueAa, :idAlbumAa, :numeroPiste);");
+  $req->bindParam(':idMusiqueAa', $idMusiqueAa, PDO::PARAM_INT);
+  $req->bindParam(':idAlbumAa', $idAlbumAa, PDO::PARAM_INT);
+  $req->bindParam(':numeroPiste', $numeroPiste, PDO::PARAM_INT);
+  $reqOk = $req->execute();
+  return $reqOk;
+}
+
+function supprimer_assembler_album_tous($db, $idAlbumAa) {
+  $req = $db->prepare("DELETE FROM Assembler_Album WHERE idMusiqueAa=:idMusiqueAa;");
+  $req->bindParam(':idMusiqueAa', $idAlbumAa, PDO::PARAM_INT);
   $reqOk = $req->execute();
   return $reqOk;
 }
