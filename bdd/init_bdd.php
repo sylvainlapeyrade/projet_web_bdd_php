84c02cd663202d9f749b717ab4fe02d7bd62ead6
Sylvain <sylvain.lapeyrade@hotmail.fr>
2018-05-07 15:43:24 +0200

    -Refactorisation init_bdd.php

--- bdd/init_bdd.php
+++ bdd/init_bdd.php
@@ -2,7 +2,7 @@
 
 include_once(dirname(__FILE__).'/connexion.php');
 
-var_dump($db->exec(file_get_contents('./structure.sql')));
-var_dump($db->exec(file_get_contents('./population.sql')));
-
-?>
+if ( isset($db)){
+    var_dump($db->exec(file_get_contents('./structure.sql')));
+    var_dump($db->exec(file_get_contents('./population.sql')));
+}
