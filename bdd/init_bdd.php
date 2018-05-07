6f0a8bdd6e559d4b0786f4733ea9c49801b623b1
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-07 16:39:56 +0200

    Maj:

--- bdd/init_bdd.php
+++ bdd/init_bdd.php
@@ -2,6 +2,8 @@
 
 include_once(dirname(__FILE__).'/connexion.php');
 
+$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
+
 var_dump($db->exec(file_get_contents('./structure.sql')));
 var_dump($db->exec(file_get_contents('./population.sql')));
 
