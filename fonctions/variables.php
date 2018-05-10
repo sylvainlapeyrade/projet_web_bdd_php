fe53a2680d603fb28bc7e20ac6b9a595ac42a932
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-10 16:31:14 +0200

    Ajout fonction format pour la durée des musiques

--- fonctions/variables.php
+++ fonctions/variables.php
@@ -7,25 +7,31 @@ function format_date($date) {
     return null;
 }
 
-    $info = [
-        'head' => [
-            'title' => "Mon projet"
+function format_duree($secondes) {
+	$minutes = intdiv($secondes,60);
+	$secondes = $secondes % 60;
+	return("$minutes min $secondes s");
+}
+
+$info = [
+    'head' => [
+        'title' => "Mon projet"
+    ],
+    'bdd' => [
+        'postgres' => [
+            'db_name' => 'bd_web',
+            'db_host' => 'localhost',
+            'db_user' => 'postgres',
+            'db_pwd' => '1234567890'
+
         ],
-        'bdd' => [
-            'postgres' => [
-                'db_name' => 'bd_web',
-                'db_host' => 'localhost',
-                'db_user' => 'postgres',
-                'db_pwd' => '1234567890'
-                
-            ],
-            'mysql' => [
-                'db_name' => 'bd_web',
-                'db_host' => 'localhost',
-                'db_user' => 'user',
-                'db_pwd' => '1234567890'   
-            ]
+        'mysql' => [
+            'db_name' => 'bd_web',
+            'db_host' => 'localhost',
+            'db_user' => 'user',
+            'db_pwd' => '1234567890'   
         ]
-    ];
+    ]
+];
 
 ?>
