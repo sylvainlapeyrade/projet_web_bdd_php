0f95c36bfd3620438738c6a3c3fa69fea45ca1d5
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-10 14:05:58 +0200

    Madification texte footer

--- footer.php
+++ footer.php
@@ -1,5 +1,3 @@
 <footer class="t10 flex flex-center text-center">
-    OLIVIER - LAPEYRADE - LABORDE - ROBERT DE ST VINCENT - WATHOJE
-    <br>
     Critique musical © <?php echo date('Y'); ?>
 </footer>
