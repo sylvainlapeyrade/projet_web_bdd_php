189de6ffc7d9a49e644b009dab016002eb6a318c
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-13 18:03:44 +0200

    Page résultat recherche

--- fonctions/fonctionRecherche.php
+++ fonctions/fonctionRecherche.php
@@ -2,16 +2,63 @@
 
 /**
  * Renvoie les artistes à partir de leur nom, prénom ou nom de scène
+ * On utilise les joker % avant d'utiliser bindParam
  * @param $db PDO Instance PDO de connexion à la BDD
  * @param $recherche String Chaîne à rechercher
  * @return array Artistes correspondant à la recherche
  */
-function rechercher($db, $recherche) {
-    $req = $db->prepare("SELECT * FROM Artiste 
-WHERE nomArtiste=:nomArtiste OR prenomArtiste=:prenomArtiste OR nomscene=:nomScene");
-    $req->bindParam(':nomArtiste', $recherche);
-    $req->bindParam(':prenomArtiste', $recherche);
-    $req->bindParam(':nomScene', $recherche);
+function rechercher_artiste($db, $recherche) {
+    $recherche = "%$recherche%";
+    $req = $db->prepare("SELECT * FROM Artiste WHERE nomArtiste LIKE :recherche OR prenomArtiste LIKE :recherche OR nomScene LIKE :recherche;");
+    $req->bindParam(':recherche', $recherche);
+    $req->execute();
+    $res = $req->fetchAll();
+    return $res;
+}
+
+/**
+ * Renvoie les groupes à partir de leur nom
+ * On utilise les joker % avant d'utiliser bindParam
+ * @param $db PDO Instance PDO de connexion à la BDD
+ * @param $recherche String Chaîne à rechercher
+ * @return array Groupes correspondant à la recherche
+ */
+function rechercher_groupe($db, $recherche) {
+    $recherche = "%$recherche%";
+    $req = $db->prepare("SELECT * FROM Groupe WHERE nomGroupe LIKE :recherche;");
+    $req->bindParam(':recherche', $recherche);
+    $req->execute();
+    $res = $req->fetchAll();
+    return $res;
+}
+
+/**
+ * Renvoie les albums à partir de leur nom
+ * On utilise les joker % avant d'utiliser bindParam
+ * @param $db PDO Instance PDO de connexion à la BDD
+ * @param $recherche String Chaîne à rechercher
+ * @return array Albums correspondant à la recherche
+ */
+function rechercher_album($db, $recherche) {
+    $recherche = "%$recherche%";
+    $req = $db->prepare("SELECT * FROM Album WHERE nomAlbum LIKE :recherche;");
+    $req->bindParam(':recherche', $recherche);
+    $req->execute();
+    $res = $req->fetchAll();
+    return $res;
+}
+
+/**
+ * Renvoie les musiques à partir de leur nom
+ * On utilise les joker % avant d'utiliser bindParam
+ * @param $db PDO Instance PDO de connexion à la BDD
+ * @param $recherche String Chaîne à rechercher
+ * @return array Musiques correspondant à la recherche
+ */
+function rechercher_musique($db, $recherche) {
+    $recherche = "%$recherche%";
+    $req = $db->prepare("SELECT * FROM Musique, Composer_Musique, Artiste WHERE titreMusique LIKE :recherche AND Musique.idMusique=Composer_Musique.idMusiqueCoMu AND Composer_Musique.idArtisteCoMu=Artiste.idArtiste;");
+    $req->bindParam(':recherche', $recherche);
     $req->execute();
     $res = $req->fetchAll();
     return $res;
---
+++ recherche.php
@@ -0,0 +1,116 @@
+<?php
+
+session_start();
+include_once(dirname(__FILE__).'/fonctions/variables.php');
+include_once(dirname(__FILE__).'/fonctions/fonctionCompte.php');
+include_once(dirname(__FILE__).'/fonctions/fonctionRecherche.php');
+include_once(dirname(__FILE__).'/bdd/connexion.php');
+
+$info['head']['subTitle'] = "Resultat de recherche";
+$info['head']['stylesheets'] = ['recherche.css'];
+
+$recherche = $_GET['recherche'];
+if ( isset($db, $recherche) ) {
+    $listeArtistesRecherche = rechercher_artiste($db, $recherche);
+    $listeGroupesRecherche = rechercher_groupe($db, $recherche);
+    $listeAlbumsRecherche = rechercher_album($db, $recherche);
+    $listeMusiquesRecherche = rechercher_musique($db, $recherche);
+} else {
+    header('Location: /index.php');
+}
+
+include_once(dirname(__FILE__).'/head.php');
+
+?>
+
+<?php include_once(dirname(__FILE__).'/header.php'); ?>
+
+<main>
+    <section>
+        <div id="page-resultat-recherche">
+            <?php if ( !empty($listeArtistesRecherche) ) { ?>
+                <div>
+                    <h3>ARTISTES correspondants à votre recherche "<?php echo $recherche ?>"</h3>
+                    <hr color="white" size="1">
+                    <?php foreach($listeArtistesRecherche as $artiste) { ?>
+                        <div class="flex">
+                            <img class="image-recherche" src="<?php echo $artiste['urlimageartiste']; ?>" />
+                            <div class="information-recherche">
+                                <?php echo $artiste['nomartiste'].' '.$artiste['prenomartiste'] ?>
+                                <?php if ( !empty($artiste['nomscene']) ) { echo '('.$artiste['nomscene'].')'; } ?>
+                                <a id="boutton-voir-plus" class="button button-red1" href="/artiste.php?idArtiste=<?php echo $artiste['idartiste']; ?>">Voir les détails</a>
+                            </div>
+                        </div>
+                        <hr color="white" size="1">
+                    <?php } ?>
+                </div>
+                <hr color="white" size="2">
+            <?php } ?>
+            
+            <?php if ( !empty($listeGroupesRecherche) ) { ?>
+                <div>
+                    <h3>GROUPES correspondants à votre recherche "<?php echo $recherche ?>"</h3>
+                    <hr color="white" size="1">
+                    <?php foreach($listeGroupesRecherche as $groupes) { ?>
+                        <div class="flex">
+                            <img class="image-recherche" src="<?php echo $groupes['urlimagegroupe']; ?>" />
+                            <div class="information-recherche">
+                                <?php echo $groupe['nomgroupe']; ?>
+                                <a id="boutton-voir-plus" class="button button-red1" href="/groupe.php?idGroupe=<?php echo $groupes['idgroupe']; ?>">Voir les détails</a>
+                            </div>
+                        </div>
+                        <hr color="white" size="1">
+                    <?php } ?>
+                </div>
+                <hr color="white" size="2">
+            <?php } ?>
+            
+            <?php if ( !empty($listeAlbumsRecherche) ) { ?>
+                <div>
+                    <h3>ALBUMS correspondants à votre recherche "<?php echo $recherche ?>"</h3>
+                    <hr color="white" size="1">
+                    <?php foreach($listeAlbumsRecherche as $album) { ?>
+                        <div class="flex">
+                            <img class="image-recherche" src="<?php echo $album['urlpochettealbum']; ?>" />
+                            <div class="information-recherche">
+                                <?php echo $album['nomalbum']; ?>
+                                <br>
+                                <a id="boutton-voir-plus" class="button button-red1" href="/album.php?idAlbum=<?php echo $album['idalbum']; ?>">Voir les détails</a>
+                            </div>
+                        </div>
+                        <hr color="white" size="1">
+                    <?php } ?>
+                </div>
+                <hr color="white" size="2">
+            <?php } ?>
+            
+            <?php if ( !empty($listeMusiquesRecherche) ) { ?>
+                <div>
+                    <h3>MUSIQUES correspondants à votre recherche "<?php echo $recherche ?>"</h3>
+                    <hr color="white" size="1">
+                    <?php foreach($listeMusiquesRecherche as $musique) { ?>
+                        <div class="information-recherche">
+                            Musique : 
+                            <?php echo $musique['titremusique']; ?>
+                            <br>
+                            Autheur : 
+                            <?php if ( !empty($musique['nomscene']) ) {
+                                echo $musique['nomscene'];
+                            } else {
+                                echo $musique['nomartiste'].' '.$musique['prenomartiste'];
+                            } ?>
+                            <a id="boutton-voir-plus" class="button button-red1" href="/artiste.php?idArtiste=<?php echo $musique['idartiste']; ?>">Voir les détails</a>
+                        </div>
+                        <hr color="white" size="1">
+                    <?php } ?>
+                </div>
+                <hr color="white" size="2">
+            <?php } ?>
+            
+            
+            
+        </div>
+    </section>
+</main>
+
+<?php include_once(dirname(__FILE__).'/footer.php'); ?>
