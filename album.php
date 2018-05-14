e8ff28a37e00c1ab8481b58f477a27b8bf61d625
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-14 13:47:12 +0200

    Changement de fichier pour le code commentaire

--- album.php
+++ album.php
@@ -4,12 +4,19 @@ session_start();
 include_once(dirname(__FILE__).'/fonctions/variables.php');
 include_once(dirname(__FILE__).'/fonctions/fonctionCompte.php');
 include_once(dirname(__FILE__).'/fonctions/fonctionAlbum.php');
+include_once(dirname(__FILE__).'/fonctions/fonctionEvaluer.php');
 include_once(dirname(__FILE__).'/bdd/connexion.php');
 
 $info['head']['subTitle'] = "Page album";
 $info['head']['stylesheets'] = ['barreRecherche.css', 'album.css'];
 
 $idAlbum = $_GET['idAlbum'];
+$action = $_GET['action'];
+$star = $_GET['star'];
+if ( !isset($star) || empty($star) ) {
+    $star = 0;
+}
+
 if ( isset($db, $idAlbum) ) {
     $album = recuperer_album($db, $idAlbum)[0];
     if ( empty($album) ) {
@@ -17,6 +24,7 @@ if ( isset($db, $idAlbum) ) {
     }
     $listeArtistesAlbum = recuperer_artiste_album($db, $idAlbum);
     $listeMusiquesAlbum = recuperer_musique_album($db, $idAlbum);
+    $listeEvaluations = recuperer_evaluation_album_tous($db, $idAlbum);
 } else {
     header('Location: /index.php');
 }
@@ -33,7 +41,7 @@ include_once(dirname(__FILE__).'/head.php');
         
         <?php include_once(dirname(__FILE__).'/barreRecherche.php'); ?>
         
-        <!-- Présentation de l'album -->
+        <!-- PRESENTATION -->
         <div id="page-album">
             <div class="flex flex-between">
                 <div id="description-album" class="flex-around">
@@ -93,8 +101,58 @@ include_once(dirname(__FILE__).'/head.php');
                 </div>
                 
             </div>
+            <!-- FIN PRESENTATION -->
             
-            <?php include_once(dirname(__FILE__).'/commentaireAlbum.php'); ?>
+            <div id="liste-commentaire">
+                <hr size="1" color=#e8491d>
+
+                <? if ( isset($erreur) ) { ?>
+                    <div class="red"><?php echo $erreurCommentaire; ?></div>
+                <? } ?>
+
+                <?php if ( is_connect() ) { ?>
+                    <form action="/album.php" method="get">
+                        <div class="rating">
+                            <p id="stars"> Donnez une note à cette musique :
+                                <a class="<?php if ($star>0) echo 'red' ?>" href="?idAlbum=<?php echo $idAlbum; ?>&star=1#stars">★</a>
+                                <a class="<?php if ($star>1) echo 'red' ?>" href="?idAlbum=<?php echo $idAlbum; ?>&star=2#stars">★</a>
+                                <a class="<?php if ($star>2) echo 'red' ?>" href="?idAlbum=<?php echo $idAlbum; ?>&star=3#stars">★</a>
+                                <a class="<?php if ($star>3) echo 'red' ?>" href="?idAlbum=<?php echo $idAlbum; ?>&star=4#stars">★</a>
+                                <a class="<?php if ($star>4) echo 'red' ?>" href="?idAlbum=<?php echo $idAlbum; ?>&star=5#stars">★</a>
+                            </p>
+                        </div>
+                        <input type="hidden" name="idAlbum" value="<?php echo $idAlbum ?>" />
+                        <input type="hidden" name="action" value="ajouterEvaluation" />
+                        <textarea class="input-area" name="comments" cols="50" rows="5" placeholder="Votre commentaire ici..."></textarea>
+                        <input class="bouton bouton-forme1 bouton-red1" type="reset" value="Réinitialiser">
+                        <input class="bouton bouton-forme1 bouton-red1" type="submit" value="Envoyer">
+                    </form>
+                <?php } ?>
+
+                <div class="comment">
+                    <h2> Commentaires </h2>
+                    <hr size="1" color=#e8491d>
+
+                    <div>
+                        <p>
+                            <b>Identifiant</b>&nbsp; &nbsp;21 Mai 2017
+                            <?php if ( is_admin() ) { ?> <a class="bouton bouton-forme2 bouton-red1">Supprimer</a> <?php } ?>
+                        </p>
+                        <p>Ceci est un commentaire. Pour l'instant, ce n'est qu'un test. On revient automatiquement à la ligne si le commentaire est trop long...</p>
+                        <hr size="1" color=#e8491d>
+                    </div>
+
+                    <div>
+                        <p>
+                        <b>Identifiant</b>&nbsp; &nbsp;25 Avril 2018
+                            <?php if ( is_admin() ) { ?> <a class="bouton bouton-forme2 bouton-red1">Supprimer</a> <?php } ?>
+                        </p>
+                        <p>Ceci est un commentaire. Pour l'instant, ce n'est qu'un test. On revient automatiquement à la ligne si le commentaire est trop long...</p>
+                        <hr size="1" color=#e8491d>
+                    </div>
+                </div>
+            </div>
+            <!-- FIN COMMENTAIRE -->
             
         </div>
         
