f4c72242f68d48f3b7cff5fef9155ba3fcc3a31a
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-17 17:48:32 +0200

    Style

--- aPropos.php
+++ aPropos.php
@@ -6,7 +6,7 @@ include_once(dirname(__FILE__).'/fonctions/fonctionCompte.php');
 include_once(dirname(__FILE__).'/bdd/connexion.php');
 
 $info['head']['subTitle'] = "A propos";
-$info['head']['stylesheets'] = ['accueil.css'];
+$info['head']['stylesheets'] = ['accueil.css', 'aPropos.css'];
 
 include_once(dirname(__FILE__).'/head.php');
 
@@ -15,28 +15,28 @@ include_once(dirname(__FILE__).'/head.php');
 <?php include_once(dirname(__FILE__).'/header.php'); ?>
 
 <main>
-    <section id="page-apropos">
+    <section>
 
-        <div>
-            <h1 class="red1 t35 text-center">Le projet Web</h1>
-            <h2 class="white  t20 flex flex-center">
-                Ce site est le produit d'un projet de la filière Systèmes de Télécommunications, Réseaux et Informatique. Notre groupe composé de 5 étudiants
-                avait comme but de réaliser un site fonctionnel de critiques sur des oeuvres musicales. Ainsi, le site internet sur lequel vous naviguez en ce moment même
-                est le résultat de semaines de travail sur les langages HTML, CSS, PHP ainsi que sur l'utilisation d'une base de données.
-            </h2>
-        </div>
-        
-        <div>
-            <h1 class="red1 t35 text-center"> L'équipe </h1>
-            <h2 class="white  t20 flex flex-center">
+        <div id="page-apropos" class="t18 text-center">
+            <div>
+                <h1 class="red1 t35">Le projet Web</h1>
+                <div>
+                    Ce site est le produit d'un projet de la filière Systèmes de Télécommunications, Réseaux et Informatique. Notre groupe composé de 5 étudiants
+                    avait comme but de réaliser un site fonctionnel de critiques sur des oeuvres musicales. Ainsi, le site internet sur lequel vous naviguez en ce moment même
+                    est le résultat de semaines de travail sur les langages HTML, CSS, PHP ainsi que sur l'utilisation d'une base de données.
+                </div>
+            </div>
+
+            <div>
+                <h1 class="red1 t35"> L'équipe </h1>
                 <ul>
-                  <li> LABORDE Tonin </li>
-                  <li> LAPEYRADE Sylvain </li>
-                  <li> OLIVIER Thomas </li>
-                  <li> ROBERT DE ST VINCENT Guillaume </li>
-                  <li> WATHOJE Emmanuel </li>
+                    <li> LABORDE Tonin </li>
+                    <li> LAPEYRADE Sylvain </li>
+                    <li> OLIVIER Thomas </li>
+                    <li> ROBERT DE ST VINCENT Guillaume </li>
+                    <li> WATHOJE Emmanuel </li>
                 </ul>
-              </h2>
+            </div>
         </div>
         
     </section>
--- index.php
+++ index.php
@@ -15,18 +15,21 @@ include_once(dirname(__FILE__).'/head.php');
 <?php include_once(dirname(__FILE__).'/header.php'); ?>
 
 <main>
-    <section id="page-accueil">
-        <h1 class="white t35 text-center">Critiquez vos albums et musiques préférées</h1>
+    <section>
         
-        <form class="flex flex-center" action="/recherche.php" method="get">
-            <input id="input-recherche"
-                   type="text"
-                   class="width-500"
-                   name="recherche"
-                   placeholder="Artiste, groupe, album ou musique"
-                   />
-            <input class="bouton bouton-forme1 bouton-red1" type="submit" value="Rechercher" />
-        </form>
+        <div id="page-accueil">
+            <h1 class="white t35 text-center">Critiquez vos albums et musiques préférées</h1>
+
+            <form class="flex flex-center" action="/recherche.php" method="get">
+                <input id="input-recherche"
+                       type="text"
+                       class="width-500"
+                       name="recherche"
+                       placeholder="Artiste, groupe, album ou musique"
+                       />
+                <input class="bouton bouton-forme1 bouton-red1" type="submit" value="Rechercher" />
+            </form>
+        </div>
         
     </section>
 </main>
