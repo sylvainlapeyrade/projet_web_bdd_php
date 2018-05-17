21cb48f5b465c8533e2e0f84dda3a2f065222a93
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-17 17:12:24 +0200

    Intégration page A propos avec style

--- aPropos.php
+++ aPropos.php
@@ -1,56 +1,45 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php
 
-        <!-- Affichage du titre de la page -->
-        <title>
-            A propos de nous        </title>
+session_start();
+include_once(dirname(__FILE__).'/fonctions/variables.php');
+include_once(dirname(__FILE__).'/fonctions/fonctionCompte.php');
+include_once(dirname(__FILE__).'/bdd/connexion.php');
 
-        <!-- Affichage des STYLESHEETS -->
-        <link rel="stylesheet" href="/styles/defaut.css">
-        <link rel="stylesheet" href="/styles/header.css">
-        <link rel="stylesheet" href="/styles/footer.css">
-        <link rel="stylesheet" href="/styles/accueil.css">
+$info['head']['subTitle'] = "A propos";
+$info['head']['stylesheets'] = ['accueil.css'];
 
+include_once(dirname(__FILE__).'/head.php');
 
-    </head>
-    
-<body>
-<header class="flex flex-center">
-    <nav class="flex flex-between">
-        <ul class="flex">
-            <a class="t25" href="/index.php"><li>Critique <span class="white">Musicale</span></li></a>
-        </ul>
-        <ul class="flex">
-            <a class="button flex item-center" href="/index.php"><li>Accueil</li></a>
-                            <a class="button flex item-center" href="/compte/connexion.php"><li>Connexion</li></a>
-                <a class="button flex item-center" href="/compte/inscription.php"><li>Inscription</li></a>
-                        <a class="button flex item-center" href="/index.php"><li>à propos</li></a>
-        </ul>
-    </nav>
-</header>
+?>
+
+<?php include_once(dirname(__FILE__).'/header.php'); ?>
 
 <main>
-    <section id="content-recherche">
-        <h1 class="red1 t35 text-center">Le projet Web</h1>
-        <h2 class="white  t20 flex flex-center">
-            Ce site est le produit d'un projet de la filière Systèmes de Télécommunications, Réseaux et Informatique. Notre groupe composé de 5 étudiants
-            avait comme but de réaliser un site fonctionnel de critiques sur des oeuvres musicales. Ainsi, le site internet sur lequel vous naviguez en ce moment même
-            est le résultat de semaines de travail sur les langages HTML, CSS, PHP ainsi que sur l'utilisation d'une base de données.
-        </h2>
-        <h1 class="red1 t35 text-center"> L'équipe </h1>
-        <h2 class="white  t20 flex flex-center">
-            <ul>
-              <li> LABORDE Tonin </li>
-              <li> LAPEYRADE Sylvain </li>
-              <li> OLIVIER Thomas </li>
-              <li> ROBERT DE ST VINCENT Guillaume </li>
-              <li> WATHOJE Emmanuel </li>
-            </ul>
-          </h2>
+    <section id="page-apropos">
+
+        <div>
+            <h1 class="red1 t35 text-center">Le projet Web</h1>
+            <h2 class="white  t20 flex flex-center">
+                Ce site est le produit d'un projet de la filière Systèmes de Télécommunications, Réseaux et Informatique. Notre groupe composé de 5 étudiants
+                avait comme but de réaliser un site fonctionnel de critiques sur des oeuvres musicales. Ainsi, le site internet sur lequel vous naviguez en ce moment même
+                est le résultat de semaines de travail sur les langages HTML, CSS, PHP ainsi que sur l'utilisation d'une base de données.
+            </h2>
+        </div>
+        
+        <div>
+            <h1 class="red1 t35 text-center"> L'équipe </h1>
+            <h2 class="white  t20 flex flex-center">
+                <ul>
+                  <li> LABORDE Tonin </li>
+                  <li> LAPEYRADE Sylvain </li>
+                  <li> OLIVIER Thomas </li>
+                  <li> ROBERT DE ST VINCENT Guillaume </li>
+                  <li> WATHOJE Emmanuel </li>
+                </ul>
+              </h2>
+        </div>
+        
     </section>
 </main>
 
-<footer class="t10 flex flex-center text-center">
-    Critique musicale © 2018</footer>
+<?php include_once(dirname(__FILE__).'/footer.php'); ?>
