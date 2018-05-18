93afaf82f65485631984f2c9bbd6d79dfae9ed9a
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-18 11:52:33 +0200

    Indetation code

--- album.php
+++ album.php
@@ -45,16 +45,18 @@ include_once(dirname(__FILE__).'/head.php');
                 <div id="description-album" class="flex-around">
 
                     <div>
-                        <h1 class="red1"><a><?php if(isset($album)) {echo $album['nomalbum'];} ?></a> -
-                            <a><?php if(isset($album)) {echo $album['datealbum'];} ?></a></h1>
+                        <h1 class="red1"><a><?php if(isset($album)) {echo $album['nomalbum'];} ?></a> - <a><?php if(isset($album)) {echo $album['datealbum'];} ?></a></h1>
+                        
                         <p>
                             <?php if(isset($album)) {echo $album['descriptionalbum'];} ?>
                             <br>
                         </p>
+                        
                         <p>
                             Il a été composé par:
                             <?php foreach($listeArtistesAlbum as $key => $artiste) { ?>
                                 <a class="souligner" href="/artiste.php?idArtiste=<?php echo $artiste['idartiste']; ?>">
+                                    <!-- Affichage soit le nom de scène soit le nom/prénom -->
                                     <?php if ( !empty($artiste['nomscene']) ) {
                                         echo $artiste['nomscene'];
                                     } else {
