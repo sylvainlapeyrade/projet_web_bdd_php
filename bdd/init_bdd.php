c556650b427e0d5e3eb27bac048ee91974ed858b
Lanxor <Thomas.olivier3@yahoo.fr>
2018-05-18 17:15:51 +0200

    Modification mineur

--- bdd/init_bdd.php
+++ bdd/init_bdd.php
@@ -5,7 +5,6 @@ include_once(dirname(__FILE__).'/connexion.php');
 if ( isset($db)){
     $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING );
     var_dump($db->exec(file_get_contents('./structure.sql')));
-    var_dump($db->exec(file_get_contents('./population.sql')));
 }
 
 ?>
