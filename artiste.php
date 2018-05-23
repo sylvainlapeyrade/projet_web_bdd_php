6af5741c11bb024e9d7cedafd22d54d47861db84
Tonin <toninlaborde@hotmail.fr>
2018-05-23 10:49:33 +0200

    Update artiste.php

--- artiste.php
+++ artiste.php
@@ -92,7 +92,7 @@ include_once(dirname(__FILE__).'/head.php');
                         </table>
                             
                         <?php if ( empty($listeRecompensesArtiste) ) { ?>
-                            <h3>Aucune récompense n'a été renseignée pour cet artiste.</h3>
+                            <h3>Aucune récompense renseignée pour cet artiste.</h3>
                         <?php } ?>
                     </div>
                 </div>
